b5cd7ecf4412d3adc283e4172c7faf4a3ccb89f7
Fengyuan (Harry) Liu <fengyuan6666@gmail.com>
2026-07-17 17:34:38 -0700

    Dication page bug fix

--- frontend/src/Pages/Dictation.jsx
+++ frontend/src/Pages/Dictation.jsx
@@ -1,27 +1,8 @@
 import { useEffect, useState, useRef } from "react";
-
 import { useAppContext } from "../common/AppContext.jsx";
-
-
 import { NEXT_STATUS } from "../common/constants.js";
-
-
 import HanziWriter from "hanzi-writer";
-
-
-const [showAnswers, setShowAnswers] = useState(false);
-const [hanzi, setHanzi] = useState([""]);
-
-
-useEffect(() => {
-    
-
-    setShowAnswers(false);
-    setShowFibAnswers(false);
-    setShowUnitFibAnswers(false);
-
-}, []);
-
+import HanziDisplay from "../Components/HanziDisplay.jsx"
 
 function Reset(){
     setShowAnswers(false);
@@ -29,6 +10,18 @@ function Reset(){
 }
 
 function Dictation(){
+    const {cohortCharacters} = useAppContext();
+    const [showAnswers, setShowAnswers] = useState(false);
+    const [hanzi, setHanzi] = useState("");
+    const [useCohort, setUseCohort] = useState(true);
+
+    useEffect(() => {
+        
+
+        setShowAnswers(false);
+        
+
+    }, []);
 
     const { createRound, 
         startNextUnit, 
@@ -53,17 +46,18 @@ function Dictation(){
     const appcontext = useAppContext();
     return(
         <div>
-            <input
-                type="text"
-                value = {hanzi ?? ""}
-                onChange = {(change) => setHanzi(change.target.value)}
-            /> 
-
             <h1>Listen and write down each character / word</h1>
-            <button onClick={() => setShowAnswers(!showAnswers)}>show answers!</button>
+            {!useCohort && (
+                <input
+                type="text"
+                value = {hanzi}
+                onChange = {(change) => setHanzi(change.target.value)}
+                /> 
+            )}
+            <button onClick={() => setShowAnswers(!showAnswers)}>{showAnswers ? "hided answers" : "show answers"}</button>
             
-            {showAnswers && hanzi.map((character) => (
-                <HanziDisplay key={character.id ?? character.hanzi} hanzi={character.hanzi} />
+            {showAnswers && hanzi.map((character, i) => (
+                <HanziDisplay key={i} hanzi={character} />
             ))}
             
             <button onClick={Reset}>Reset!</button>
